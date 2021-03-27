e2ae4b98c21cbddbbf2a473367fafc35a0fb5162
Erdinc Albayrak <heyerdinc@gmail.com>
2021-03-27 06:19:27 -0700

    Print IP address of docker container

--- create_database.sh
+++ create_database.sh
@@ -1,4 +1,5 @@
-docker run --name docker_postgres -e POSTGRES_PASSWORD=123456 -d -v /tmp/postgres:/var/lib/postgresql/data postgres
+docker run --rm --name docker_postgres -e POSTGRES_PASSWORD=123456 -d -v /tmp/postgres:/var/lib/postgresql/data postgres
 docker cp './init.sql' docker_postgres:/tmp/init.sql
-docker exec -it docker_postgres psql -U postgres -a -f '/tmp/init.sql' 
+docker exec docker_postgres psql -U postgres -a -f '/tmp/init.sql' 
+docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' docker_postgres
 
